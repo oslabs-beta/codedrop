05ec24b496a89516c7a0b4143bfd9525ca2b20f3
dyeoman2 <dyeoman2@gmail.com>
2021-08-19 09:59:08 -0700

    added code drawer and prettier api

---
+++ components/CodeDrawer.jsx
@@ -0,0 +1,59 @@
+import { useState, Fragment } from 'react';
+import Box from '@material-ui/core/Box';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import CodeIcon from '@material-ui/icons/Code';
+
+import Editor from './Editor';
+import ReactIcon from './util/Icons/ReactIcon';
+
+export default function CodeDrawer({ codeString }) {
+  const [showCode, setShowCode] = useState(false);
+  const drawerDirection = 'bottom';
+
+  const toggleDrawer = (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) return;
+    setShowCode(!showCode);
+  };
+
+  const flexContainer = {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 0,
+    margin: 0,
+  };
+
+  const box = {
+    height: '345px',
+  };
+
+  const list = (anchor) => (
+    <Box role="presentation" style={box}>
+      <List style={flexContainer}>
+        {['React'].map((text, index) => (
+          <ListItem button key={text}>
+            <ReactIcon />
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <Editor language="js" displayName="React" value={codeString} readOnly={true} />
+    </Box>
+  );
+
+  return (
+    <Fragment key={drawerDirection}>
+      <Button onClick={toggleDrawer} variant="contained" color="secondary" startIcon={<CodeIcon />}>
+        View Code
+      </Button>
+      <Drawer anchor={drawerDirection} open={showCode} onClose={toggleDrawer}>
+        {list(drawerDirection)}
+      </Drawer>
+    </Fragment>
+  );
+}
---
+++ pages/api/prettier.js
@@ -0,0 +1,11 @@
+export default function prettier(req, res) {
+  const prettier = require('prettier')
+  try {
+    const { codeString } = req.body
+    const formattedCode = prettier.format(codeString)
+    res.status(200).json({ code: formattedCode });
+  } catch (e) {
+    console.log('prettier api error: ', e)
+    res.status(500).send(e)
+  }
+}
