eb1afc57769ff8ccf9cda5c647c6697b66ddda91
dyeoman2 <dyeoman2@gmail.com>
2021-08-21 13:06:52 -0700

    use row id for the key instead of name for projects

--- pages/projects.js
+++ pages/projects.js
@@ -147,7 +147,7 @@ export default function EnhancedTable() {
                       role="checkbox"
                       aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.name}
+                      key={row.id}
                       selected={isItemSelected}
                     >
                       <TableCell padding="checkbox">
