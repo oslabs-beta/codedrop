8309bdb0eb408dab2d4d004b45e3d5f0bf621268
aramay <kasper_341@hotmail.com>
2021-09-20 18:35:17 -0700

    Add React & Angular classes to generate code

--- helpers/codeGen/htmlGenerator.js
+++ helpers/codeGen/htmlGenerator.js
@@ -3,32 +3,81 @@ import { removeLinebreaks } from './removeLinebreaks';
 
 export const htmlGenerator = (fullComponentDetails, framework) => {
   const { containerStyle, id, style, value } = fullComponentDetails;
-  const inlineContainerStyle = parseCSSText(removeLinebreaks(containerStyle));
-  const inlineStyle = parseCSSText(removeLinebreaks(style));
+  const inlineContainerStyleWoLineBreaks = removeLinebreaks(containerStyle)
+  const inlineContainerStyle = parseCSSText(inlineContainerStyleWoLineBreaks);
+  const inlineStyleWoLineBreaks = removeLinebreaks(style);
+  const inlineStyle = parseCSSText(inlineStyleWoLineBreaks);
 
-  const generateChildHtml = () => {
-    if (fullComponentDetails.type === 'H1') {
-      return `<h1 style={${inlineStyle}}>${value}</h1>`;
+  class React {
+    constructor() {}
+    getBody(){
+      const generateChildHtml = () => {
+        if (fullComponentDetails.type === 'H1') {
+          return `<h1 style={${inlineStyle}}>${value}</h1>`;
+        }
+        if (fullComponentDetails.type === 'H2') {
+          return `<h2 style={${inlineStyle}}>${value}</h1>`;
+        }
+        if (fullComponentDetails.type === 'Img') {
+          return `<img class="fit-picture" src='${src}' alt='${value}' style={${inlineStyle}} />`;
+        }
+        if (fullComponentDetails.type === 'Button') {
+          return `<button style={${inlineStyle}} type='' className='' id='${id}'>${value}</button>`;
+        }
+        if (fullComponentDetails.type === 'Text') {
+          return `<span style={${inlineStyle}}>${value}</span>`;
+        }
+        if (fullComponentDetails.type === 'Input') {
+          return `<input style={${inlineStyle}}>${value}</input>`;
+        }
+      };
+      return `<div style={${inlineContainerStyle}}>${generateChildHtml()}</div>`;
     }
-    if (fullComponentDetails.type === 'H2') {
-      return `<h2 style={${inlineStyle}}>${value}</h1>`;
+  }
+  
+  class Angular {
+    constructor() {}
+    getBody(){
+      const generateChildHtml = () => {
+        if (fullComponentDetails.type === 'H1') {
+          return `<h1 style="${inlineStyleWoLineBreaks}">${value}</h1>`;
+        }
+        if (fullComponentDetails.type === 'H2') {
+          return `<h2 style="${inlineStyleWoLineBreaks}">${value}</h1>`;
+        }
+        if (fullComponentDetails.type === 'Img') {
+          return `<img class="fit-picture" src='${src}' alt='${value}' style="${inlineStyleWoLineBreaks}" />`;
+        }
+        if (fullComponentDetails.type === 'Button') {
+          return `<button style="${inlineStyleWoLineBreaks}" type='' className='' id='${id}'>${value}</button>`;
+        }
+        if (fullComponentDetails.type === 'Text') {
+          return `<span style="${inlineStyleWoLineBreaks}">${value}</span>`;
+        }
+        if (fullComponentDetails.type === 'Input') {
+          return `<input style="${inlineStyleWoLineBreaks}">${value}</input>`;
+        }
+      };
+      return `<div style="${inlineContainerStyleWoLineBreaks}">${generateChildHtml()}</div>`;
     }
-    if (fullComponentDetails.type === 'Img') {
-      return `<img class="fit-picture" src='${src}' alt='${value}' style={${inlineStyle}} />`;
-    }
-    if (fullComponentDetails.type === 'Button') {
-      return `<button style={${inlineStyle}} type='' className='' id='${id}'>${value}</button>`;
-    }
-    if (fullComponentDetails.type === 'Text') {
-      return `<span style={${inlineStyle}}>${value}</span>`;
-    }
-    if (fullComponentDetails.type === 'Input') {
-      return `<input style={${inlineStyle}}>${value}</input>`;
-    }
-  };
+  }
 
-  if (framework === "Angular") return `<div style="${inlineContainerStyle}">${generateChildHtml()}</div>`;
+  const frameworks = {
+    'Angular': Angular,
+    'React': React,
+  }
 
-  return `<div style={${inlineContainerStyle}}>${generateChildHtml()}</div>`;
+  class GenerateCode {
+    constructor(framework) {
+      this.framework = new frameworks[framework]() 
+    } 
+    getBody() {
+      return this.framework.getBody()
+    }
+  }
+
+  const formattedCode = new GenerateCode(framework)
+
+  return formattedCode.getBody()
 };
 
