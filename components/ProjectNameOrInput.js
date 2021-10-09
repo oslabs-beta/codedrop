4f30a50dd95a8abd0dbc665a0e302afd9c460419
dyeoman2 <dyeoman2@gmail.com>
2021-10-09 13:29:32 -0700

    pretty input styling for project edit

--- components/ProjectNameOrInput.js
+++ components/ProjectNameOrInput.js
@@ -13,11 +13,19 @@ const useStyles = makeStyles({
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'row',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+  },
+  input: {
+    border: 'none',
+    outline: 'none',
+    textAlign: 'center',
+    fontSize: '1.9rem',
+    padding: '4',
+    appearance: 'none',
+  },
 });
 
-// this component selectively renders the project name or an input to change the name, in [projectId].js 
+// this component selectively renders the project name or an input to change the name, in [projectId].js
 const ProjectNameOrInput = (props) => {
   const [session] = useSession();
   const classes = useStyles();
@@ -25,21 +33,31 @@ const ProjectNameOrInput = (props) => {
 
   return (
     <div className={classes.newProjectName}>
-        {
-        active ? 
-            <div>
-              <input value={value} onChange={inputChange} onBlur={blur} autoFocus/> 
-              <RiSaveLine style={{ height: '.75em' }} onClick={blur}/>
-            </div>
-            :
-            <div onDoubleClick={doubleClick} className={classes.projectNameContainer}>
-                {value}
-                <RiFileEditLine style={{ height: '.75em',  }} onClick={doubleClick}/>
-                {!session && (<RiSaveLine style={{ height: '.75em' }} onClick={() => router.push({pathname:'/signin', query:{ projectId }})}/>)}
-            </div>       
-        }
+      {active ? (
+        <div>
+          <input
+            value={value}
+            onChange={inputChange}
+            onBlur={blur}
+            autoFocus
+            className={classes.input}
+          />
+          <RiSaveLine style={{ height: '.75em' }} onClick={blur} />
+        </div>
+      ) : (
+        <div onDoubleClick={doubleClick} className={classes.projectNameContainer}>
+          {value}
+          <RiFileEditLine style={{ height: '.75em' }} onClick={doubleClick} />
+          {!session && (
+            <RiSaveLine
+              style={{ height: '.75em' }}
+              onClick={() => router.push({ pathname: '/signin', query: { projectId } })}
+            />
+          )}
+        </div>
+      )}
     </div>
-)
-}
+  );
+};
 
 export default ProjectNameOrInput;
