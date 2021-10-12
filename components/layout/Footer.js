2079abe53a911c0b597cbaa7bacf90f02e115a39
Emily Tschida <thetschida@gmail.com>
2021-10-12 15:23:55 -0500

    Added a Creators page, with links to linkedIn and Github

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,27 +3,36 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
 import Link from '@material-ui/core/Link';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary">
-      {'Copyright © '}
-      <Link color="inherit" href="/">
-        CodeDrop
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const useStyles = makeStyles({
   footer: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     minHeight: '50px',
     boxShadow: '0px -1px 2px rgb(0 0 0 / 20%)',
+  },
+  creatorsLink: {
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    border: '1px solid #455A64',
+    borderRadius: 4,
+    textDecoration: 'none',
+    '&:hover': {
+      backgroundColor: '#455A64',
+      color: '#FFFFFF',
+    }
+  },
+  copyright: {
+    paddingLeft: 50,
+    paddingRight: 10,
+  },
+  copyrightLink: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    textDecoration: 'none',
   },
 });
 
@@ -32,7 +41,20 @@ export default function StickyFooter() {
 
   return (
     <footer className={classes.footer}>
-      <Copyright />
+      <span className={classes.creators}>
+        <Typography variant="body2" color="textSecondary">
+          <Link className={classes.creatorsLink} color="inherit" href="creators">  
+            Creators
+          </Link>
+        </Typography>
+      </span>
+      <Typography className={classes.copyright} variant="body2" color="textSecondary">
+        {'Copyright © '}
+        <Link className={classes.copyrightLink}color="inherit" href="/">
+          CodeDrop
+        </Link>{' '}
+        {new Date().getFullYear()}
+      </Typography>
     </footer>
   );
 }
