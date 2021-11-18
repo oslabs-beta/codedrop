120368c6c31d787e76d507595ab8b3f0d025cf2e
bamche88 <76884966+bamche88@users.noreply.github.com>
2021-11-17 22:19:44 -0800

    initial testing

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByText('Increase your productivity')
+
+    expect(heading).toBeInTheDocument()
+  })
+})
